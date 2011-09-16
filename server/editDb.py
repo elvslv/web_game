6016af2804f0e9aa72f01b85c33b9cfbf2541823
Elena Vasilyeva <elvslv@gmail.com>
2011-09-17 10:43:45 +1100

    Added 'games', 'players', 'maps' tables
    Signed-off-by: Natalia Zemliannikova

--- server/editDb.py
+++ server/editDb.py
@@ -1,6 +1,8 @@
 import MySQLdb
 from misc import MAX_USERNAME_LEN
 from misc import MAX_PASSWORD_LEN
+from misc import MAX_GAMENAME_LEN
+from misc import MAX_MAPNAME_LEN
 
 DATABASE_HOST = "localhost"
 DATABASE_USER = "admin"
@@ -8,14 +10,20 @@ DATABASE_NAME = "testdb"
 DATABASE_PASSWD = "12345"
 DATABASE_PORT = 3306
 
-tables = ['Users', 'Sessions']
+tables = ['Users', 'Sessions', 'Games', 'Players', 'Maps']
 
 def createTables():
 	cursor.execute("CREATE TABLE IF NOT EXISTS \
-			Users(Id INT PRIMARY KEY AUTO_INCREMENT, UserName VARCHAR(%d) UNIQUE, Password VARCHAR(%d))" % 
-                (MAX_USERNAME_LEN, MAX_PASSWORD_LEN))
+			Users(Id INT PRIMARY KEY AUTO_INCREMENT, UserName VARCHAR(%d) UNIQUE, Password VARCHAR(%d))" % (MAX_USERNAME_LEN, MAX_PASSWORD_LEN))
 	cursor.execute("CREATE TABLE IF NOT EXISTS \
 			Sessions(Sid INT PRIMARY KEY AUTO_INCREMENT, UserId INT UNIQUE, Start DATETIME)")
+	cursor.execute("CREATE TABLE IF NOT EXISTS \
+			Games(GameId INT UNSIGNED PRIMARY KEY, GameName VARCHAR(%d), MapId INT UNSIGNED, PlayersNum INT UNSIGNED, State TINYINT)" % MAX_GAMENAME_LEN)
+	cursor.execute("CREATE TABLE IF NOT EXISTS \
+			Players(Sid INT UNSIGNED PRIMARY KEY, GameId INT UNSIGNED, Readiness TINYINT(1), CurrentRace TINYINT UNSIGNED, \
+			DeclineRace TINYINT UNSIGNED, Coins TINYINT UNSIGNED)")
+	cursor.execute("CREATE TABLE IF NOT EXISTS \
+			Maps(MapId INT UNSIGNED PRIMARY KEY, MapName VARCHAR(%d))" % MAX_MAPNAME_LEN)
                 
 def clearDb():
 	for t in tables:
